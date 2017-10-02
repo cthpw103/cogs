c5efae460e53b3c363e012c6130b91508422cc0d
cth103 <cthdev@email.com>
2017-10-02 17:03:40 +0200

    Update randomcog.py

--- randomcog.py
+++ randomcog.py
@@ -19,23 +19,21 @@ class CthsRandomStuff:
         while i < l:
             t = ts[i]
             if t == ">": p += 1
-            elif t == "<": p -= 1
-            elif t == "+": c[p] += 1
-            elif t == "-": c[p] -= 1
-            elif t == ".": rv.append(chr(c[p]))
-            elif t == ",": pass
-            elif t == "[":
+            if t == "<": p -= 1
+            if t == "+": c[p] += 1
+            if t == "-": c[p] -= 1
+            if t == ".": rv.append(chr(c[p]))
+            if t == ",": pass
+            if t == "[":
                 if c[p] == 0:
                     while ts[i] != "]": i += 1
                     loop.pop()
                 else:
                 loop.append(i - 1)
-            elif t == "]": i = loop[-1]
+            if t == "]": i = loop[-1]
             i += 1
         await ctx.send("".join(rv))
-    @commands.command()
-    async def cthsrandomhelp(self, ctx):
-        await ctx.send("only >brainfuck, more is coming soon.")
+                
         
 def setup(bot):
     print("installing the best cog ever")
