5ccd5788f502313a06552dce348bab762dada116
cth103 <cthdev@email.com>
2017-10-02 18:25:11 +0200

    Update randomcog.py

--- randomcog.py
+++ randomcog.py
@@ -11,21 +11,21 @@ class CthsRandomStuff:
         
     @commands.command()
     async def ouchifell(self, ctx):
-		attachments = ctx.message.attachments
-		for attachment in attachments:
-			file = cStringIO.StringIO(urllib.urlopen(attachment['url']).read())
+                attachments = ctx.message.attachments
+                for attachment in attachments:
+                        file = cStringIO.StringIO(urllib.urlopen(attachment['url']).read())
             img = Image.open(file)
-			img = img.filter(ImageFilter.BLUR) 
-			img.save("ouch.jpg", "JPEG") 
-			await bot.upload(final, filename='ouch.jpg')
-			os.remove("ouch.jpg")
+                        img = img.filter(ImageFilter.BLUR) 
+                        img.save("ouch.jpg", "JPEG") 
+                        await bot.upload(final, filename='ouch.jpg')
+                        os.remove("ouch.jpg")
 
     @commands.command()
     async def halp(self, ctx):
-		try:
-		    bot.say("only command is >ouchifell, send image using the ouchifell command")
-		except Exception as e:
-		    awaitself.bot.say(e)
+                try:
+                    bot.say("only command is >ouchifell, send image using the ouchifell command")
+                except Exception as e:
+                    awaitself.bot.say(e)
                 
         
 def setup(bot):
