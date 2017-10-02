1bb6daf6a059d621b673a107dc4dd2a0f76ec0e0
cth103 <cthdev@email.com>
2017-10-02 18:13:02 +0200

    Update randomcog.py

--- randomcog.py
+++ randomcog.py
@@ -11,20 +11,15 @@ class CthsRandomStuff:
         
     @commands.command()
     async def ouchifell(self, ctx):
-		try:
-		    attachments = ctx.message.attachments
-			for attachment in attachments:
-				file = cStringIO.StringIO(urllib.urlopen(attachment['url']).read())
-                                img = Image.open(file)
-				img = img.filter(ImageFilter.BLUR) 
-				img.save("ouch.jpg", "JPEG") 
-			    await bot.upload(final, filename='ouch.jpg')
-				os.remove("ouch.jpg")
-				
-		except discord.errors.Forbidden:
-			await bot.say("ouch cant send files")
-		except Exception as e:
-			awaitself.bot.say(e)
+		attachments = ctx.message.attachments
+		for attachment in attachments:
+			file = cStringIO.StringIO(urllib.urlopen(attachment['url']).read())
+            img = Image.open(file)
+			img = img.filter(ImageFilter.BLUR) 
+			img.save("ouch.jpg", "JPEG") 
+			await bot.upload(final, filename='ouch.jpg')
+			os.remove("ouch.jpg")
+
     @commands.command()
     async def halp(self, ctx):
 		try:
